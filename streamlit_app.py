8e900217c742ae310aceb4f98817cfb5f10fbd9d
Rebecca Theismann <59316889+stormysea22@users.noreply.github.com>
2023-05-26 14:51:58 -0400

    Update streamlit_app.py
    remove dup text

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 # Flatten the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put the flattend data into a dataframe
