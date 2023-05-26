b6ff096a664a6511376964fc3003739250a6ca36
Rebecca Theismann <59316889+stormysea22@users.noreply.github.com>
2023-05-26 14:49:50 -0400

    Update streamlit_app.py
    added fruit_choice variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ streamlit.dataframe(fruits_to_show)
 # new section for fruity advice
 streamlit.header("Fruityvice Fruit Advice")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
